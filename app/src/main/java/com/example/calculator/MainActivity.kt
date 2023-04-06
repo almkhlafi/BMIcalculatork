7d631d2ccbefbd833e312a393e2dec4dad2a7f66
Almkhlafi <azaymabood2015@gmail.com>
2023-04-06 14:11:05 +0300

    Update

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -23,6 +23,9 @@ class MainActivity : AppCompatActivity() {
         var Underwei=findViewById<TextView>(R.id.Bunderweight)
         var img=findViewById<ImageView>(R.id.imageView)
         var clear=findViewById<Button>(R.id.clear)
+
+
+        try {
         clear.setOnClickListener(){
 
             cvHeight.hint.toString()
@@ -33,52 +36,93 @@ class MainActivity : AppCompatActivity() {
             img.setImageDrawable(r)
         }
 
-        calcu.setOnClickListener(){
+
+        calcu.setOnClickListener() {
+
+
+            try {
+
+
+                var weight = cvWeight.text.toString()
+                var wei = weight.toDouble()
+                var height = cvHeight.text.toString()
+                var hei = height.toDouble()
+                val bmi = (wei.toFloat() / (hei.toFloat() / 100.0).pow(2))
+                val formatt = String.format("%.2f", bmi).toFloat()
 
 
 
-            var weight=cvWeight.text.toString()
-                var wei=weight.toDouble()
-                var height=cvHeight.text.toString()
-                var hei=height.toDouble()
-                    val bmi=(wei.toFloat()/ (hei.toFloat() / 100.0).pow(2))
-            val formatt=String.format("%.2f",bmi).toFloat()
+                when {
 
-    try {
-
-
-            when{
-
-                formatt <  18.50-> {
-                    var img=findViewById<ImageView>(R.id.imageView)
-                    var r=resources.getDrawable(R.mipmap.yellow)
-                    cvResult.setText(formatt.toString()+" kg/m2"+"  Underweight"+img.setImageDrawable(r))
-                    Toast.makeText(applicationContext,"Normal",Toast.LENGTH_SHORT).show()
+                    formatt < 18.50 -> {
+                        var img = findViewById<ImageView>(R.id.imageView)
+                        var r = resources.getDrawable(R.mipmap.yellow)
+                        cvResult.setText(
+                            formatt.toString() + " kg/m2" + "  Underweight" + img.setImageDrawable(
+                                r
+                            )
+                        )
+                        Toast.makeText(applicationContext, "Normal", Toast.LENGTH_SHORT).show()
+                    }
+                    formatt in 18.50..24.9 -> {
+                        var img = findViewById<ImageView>(R.id.imageView)
+                        var r = resources.getDrawable(R.mipmap.green)
+                        cvResult.setText(
+                            formatt.toString() + " kg/m2" + "  Healthy" + img.setImageDrawable(
+                                r
+                            )
+                        )
+                        Toast.makeText(applicationContext, "Normal", Toast.LENGTH_SHORT).show()
+                    }
+                    formatt in 25.00..29.99 -> {
+                        var img = findViewById<ImageView>(R.id.imageView)
+                        var r = resources.getDrawable(R.mipmap.orang)
+                        cvResult.setText(
+                            formatt.toString() + " kg/m2" + "  Overweight" + img.setImageDrawable(
+                                r
+                            )
+                        )
+                        Toast.makeText(applicationContext, "Overweight", Toast.LENGTH_SHORT).show()
+                    }
+                    formatt in 29.99..1000.00 -> {
+                        var img = findViewById<ImageView>(R.id.imageView)
+                        var r = resources.getDrawable(R.mipmap.red)
+                        cvResult.setText(
+                            formatt.toString() + " kg/m2" + "  Obese" + img.setImageDrawable(
+                                r
+                            )
+                        )
+                        Toast.makeText(applicationContext, "Obese", Toast.LENGTH_SHORT).show()
+                    }
+                    else -> print("")
                 }
-               formatt in 18.50..24.9-> {
-                    var img=findViewById<ImageView>(R.id.imageView)
-                    var r=resources.getDrawable(R.mipmap.green)
-                    cvResult.setText(formatt.toString()+" kg/m2"+"  Healthy"+img.setImageDrawable(r))
-                    Toast.makeText(applicationContext,"Normal",Toast.LENGTH_SHORT).show()
-                }
-                formatt in  25.00..29.99->{
-                    var img=findViewById<ImageView>(R.id.imageView)
-                    var r=resources.getDrawable(R.mipmap.orang)
-                    cvResult.setText(formatt.toString()+" kg/m2"+"  Overweight"+img.setImageDrawable(r))
-                    Toast.makeText(applicationContext,"Overweight",Toast.LENGTH_SHORT).show()
-                }
-                formatt in 29.99 .. 1000.00 -> {
-                    var img=findViewById<ImageView>(R.id.imageView)
-                    var r=resources.getDrawable(R.mipmap.red)
-                    cvResult.setText(formatt.toString()+" kg/m2"+"  Obese"+img.setImageDrawable(r))
-                    Toast.makeText(applicationContext,"Obese",Toast.LENGTH_SHORT).show()
-                }
-                else -> {Toast.makeText(applicationContext,"Check your Input",Toast.LENGTH_SHORT).show()}
 
+            } catch (e: Exception) {
+
+                validateInput(cvHeight.toString(),cvWeight.toString())
             }
-        }catch (e: Exception){
-        Toast.makeText(applicationContext,"An Error",Toast.LENGTH_SHORT).show()
         }
+        }catch (e: Exception){
+        Toast.makeText(applicationContext,"An Error!!",Toast.LENGTH_SHORT).show()
+        }
+
+
+
+    }
+
+    private fun validateInput(weight:String?,height:String?):Boolean
+    {
+        return when{
+            weight.isNullOrEmpty()->{
+                Toast.makeText(this,"Weight is Empty",Toast.LENGTH_LONG ).show()
+                return false
+            }
+            height.isNullOrEmpty()->{
+                Toast.makeText(this,"Height is Empty",Toast.LENGTH_LONG).show()
+                return false
+            }
+            else ->{ return true;}
         }
     }
+
 }
